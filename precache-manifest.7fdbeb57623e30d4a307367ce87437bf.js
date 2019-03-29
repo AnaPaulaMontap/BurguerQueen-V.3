70fd7c7286cd1556e63e1c341116fa79a086fa34
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-03-28 22:58:07 -0300

    Updates

---
+++ precache-manifest.7fdbeb57623e30d4a307367ce87437bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac193e8f6a10086e776c",
+    "url": "/BurguerQueen-V.3.git/static/css/main.6bb31a55.chunk.css"
+  },
+  {
+    "revision": "ac193e8f6a10086e776c",
+    "url": "/BurguerQueen-V.3.git/static/js/main.ac193e8f.chunk.js"
+  },
+  {
+    "revision": "70345b7f398f55621478",
+    "url": "/BurguerQueen-V.3.git/static/js/runtime~main.70345b7f.js"
+  },
+  {
+    "revision": "2705963e0bcc32228daf",
+    "url": "/BurguerQueen-V.3.git/static/js/2.2705963e.chunk.js"
+  },
+  {
+    "revision": "d0c0deecc07ff124b6f898e92ffef898",
+    "url": "/BurguerQueen-V.3.git/index.html"
+  }
+];
